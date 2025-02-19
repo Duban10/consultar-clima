9daa55120521d42891bc96a870174699cd3693c0
Luis Duban Verjel Armesto <luisv@titamedia.com>
2025-02-18 23:24:16 -0500

    Se avanza en implementacion del proyecto

--- src/components/Hook/useWeather.ts
+++ src/components/Hook/useWeather.ts
@@ -72,7 +72,7 @@ export default function useWeather() {
       setWeather(initialState)
       try {
         const apiKey = import.meta.env.VITE_API_KEY
-        const getUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${apiKey}`
+        const getUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${apiKey}`
 
         const {data} = await axios(getUrl)
 
